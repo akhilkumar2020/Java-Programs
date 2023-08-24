b9862ebe5b3635d0aa1543086cc2992d53e98206
Akhil Kumar <142902709+akhilkumar2020@users.noreply.github.com>
2023-08-24 13:08:47 +0530

    Auto_Boxing.java

--- Auto_Boxing.java
+++ Auto_Boxing.java
@@ -3,10 +3,10 @@ public class Auto_Boxing
     public static void main(String[] args)
     {
         int num  = 10;
-        Integer obj = Integer.valueOf(num);
+        Integer obj = Integer.valueOf(num);  // Autoboxing
         
         // Integer num = new Integer(10);
-        //int num1 = num.intValue();
+        //int num1 = num.intValue();    // Unboxing 
         
         System.out.println(num + " " + obj);
     }
