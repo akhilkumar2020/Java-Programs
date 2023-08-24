f5e04bf78d8207561b36d859d967f5a7d7056cdd
Akhil Kumar <142902709+akhilkumar2020@users.noreply.github.com>
2023-08-24 13:17:09 +0530

    Method-OverLoading.java

--- Method-OverLoading.java
+++ Method-OverLoading.java
@@ -1,3 +1,14 @@
+Polymorphism in Java is a concept by which we can perform a single action in different ways. Polymorphism is derived from 2 Greek words: poly and morphs. 
+The word "poly" means many and "morphs" means forms. So polymorphism means many forms.
+There are two types of polymorphism in Java:- 
+                                            Compile-time polymorphism (method overloading) and 
+                                            Runtime polymorphism (method overriding). 
+
+Why And When To Use "Inheritance" and "Polymorphism"? - 
+                                                    It is useful for code reusability: reuse attributes and methods of an existing class when you create a 
+                                                    new class.
+
+PROGRAM
 class OverLoding_Demo
 {
     public int add( int a , int b)
