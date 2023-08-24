4471687f533ccf4944c00cedd81d63cb1a188f11
Akhil Kumar <142902709+akhilkumar2020@users.noreply.github.com>
2023-08-24 18:22:32 +0530

    This_Example1.java

--- This_Example.java
+++ This_Example.java
@@ -1,3 +1,16 @@
+The "this" keyword refers to the current object in a method or constructor.
+The most common use of the "this" keyword is to eliminate the confusion between class attributes and parameters with the same name (because a class attribute is 
+shadowed by a method or constructor parameter).
+
+this can also be used to:
+1. Invoke the current class constructor
+2. Invoke the current class method
+3. Return the current class object
+4. Pass an argument in the method call
+5. Pass an argument in the constructor call
+
+PROGRAM - "this" keyword
+    
 class This_Example
 {
     int age;
