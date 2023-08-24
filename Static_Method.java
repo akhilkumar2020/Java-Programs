7cb93f7abc423aedc4409647fe9b4e5f65061548
Akhil Kumar <142902709+akhilkumar2020@users.noreply.github.com>
2023-08-24 13:24:25 +0530

    Static_Method.java

--- Static_Method.java
+++ Static_Method.java
@@ -1,5 +1,6 @@
 STATIC METHOD – Static method means class method, so we don't need to create an object of the class, we can call the method directly.
 
+PROGRAM - Static method
 public class Static_Method
 {
     public static void show()
@@ -10,5 +11,23 @@ public class Static_Method
     public static void main(String[] abc)
     {
         show();
+    }
+}
+
+NORMAL METHOD – In the normal method, we need to create an object of the class in order to call the method.
+
+PROGRAM - Non-Static/Normal Method
+
+public class Non_Static_Method
+{
+    public void show()
+    {
+        System.out.println("Hello Static Method");
+    }
+
+    public static void main(String[] abc)
+    {
+        Non_Static_Method obj = new Non_Static_Method();
+        obj.show();
     }
 }
