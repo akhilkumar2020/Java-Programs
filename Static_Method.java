8458403fda4abbd1e1f37c4483ab6fda9d8d9218
Akhil Kumar <142902709+akhilkumar2020@users.noreply.github.com>
2023-08-24 13:21:16 +0530

    Add files via upload
    STATIC METHOD – Static method means class method, so we don't need to create an object of the class, we can call the method directly.

---
+++ Static_Method.java
@@ -0,0 +1,12 @@
+public class Static_Method
+{
+    public static void show()
+    {
+        System.out.println("Hello Static Method");
+    }
+
+    public static void main(String[] abc)
+    {
+        show();
+    }
+}
