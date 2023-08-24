1825625c50e5d5c86359f9f35ceea0133264024b
Akhil Kumar <142902709+akhilkumar2020@users.noreply.github.com>
2023-08-24 13:22:32 +0530

    Java Static_Method.java

--- Static_Method.java
+++ Static_Method.java
@@ -1,3 +1,5 @@
+STATIC METHOD – Static method means class method, so we don't need to create an object of the class, we can call the method directly.
+
 public class Static_Method
 {
     public static void show()
