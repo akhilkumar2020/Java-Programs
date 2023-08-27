9cb6b6a38ed68d3a58ca7257ef5a3f7a7fcc09cc
Akhil Kumar <142902709+akhilkumar2020@users.noreply.github.com>
2023-08-27 09:58:03 +0530

    Add files via upload

---
+++ Temp.java
@@ -0,0 +1,30 @@
+
+class Temp
+{
+	Temp()
+	{
+		// calls constructor 2
+		this(5);
+		System.out.println("The Default constructor");
+	}
+
+	// parameterized constructor 2
+	Temp(int x)
+	{
+		// calls constructor 3
+		this(5, 15);
+		System.out.println(x);
+	}
+
+	// parameterized constructor 3
+	Temp(int x, int y)
+	{
+		System.out.println(x * y);
+	}
+
+	public static void main(String args[])
+	{
+		// invokes default constructor first
+		new Temp();
+	}
+}
