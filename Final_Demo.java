428f236247b9dcc837b7b2cc18210f7df26870e8
Akhil Kumar <142902709+akhilkumar2020@users.noreply.github.com>
2023-08-28 14:16:33 +0530

    Add files via upload

---
+++ Final_Demo.java
@@ -0,0 +1,12 @@
+public class Final_Demo 
+{
+    final int num = 10;  
+    
+   
+   public static void main(String[] args) 
+   {
+    Final_Demo obj =  new Final_Demo();
+    obj.num = 20;
+    System.out.println(obj.num);
+   }
+}
