a2468ed94f51ca25102d75ae6e7385320d176870
Akhil Kumar <142902709+akhilkumar2020@users.noreply.github.com>
2023-08-28 14:17:38 +0530

    Final Keyword.java

--- Final_Demo.java
+++ Final_Demo.java
@@ -1,3 +1,10 @@
+In Java, the final keyword is used to denote constants. It can be used with variables, methods, and classes.
+Once any entity (variable, method, or class) is declared final, it can be assigned only once. That is,
+	The final variable cannot be reinitialized with another value
+	The final method cannot be overridden
+	The final class cannot be extended
+
+PROGRAM - FINAL KEYWORD
 public class Final_Demo 
 {
     final int num = 10;  
