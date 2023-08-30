5af69fc129cd0566d2a67fc616c26925d11c4500
Akhil Kumar <142902709+akhilkumar2020@users.noreply.github.com>
2023-08-30 16:32:45 +0530

    Abstraction.java

--- BMW.java
+++ BMW.java
@@ -1,3 +1,19 @@
+Data abstraction is the process of hiding certain details and showing only essential information to the user (Abstraction is a process of hiding the implementation 
+details and showing only functionality to the user). Abstraction can be achieved with either abstract classes (0-100%) or interfaces (100%). The abstract keyword 
+is a non-access modifier, used for classes and methods:
+	Abstract class: is a restricted class that cannot be used to create objects (to access it, it must be inherited from another class). An abstract class can have 
+ both abstract and regular methods.
+	Abstract method: can only be used in an abstract class, and it does not have a method body. The body is provided by the subclass (inherited from).
+
+SOME IMPORTANT POINTS SHOULD BE REMEMBERED ABOUT ABSTRACTION IN JAVA.
+•	An abstract class must be declared with an abstract keyword.
+•	It can have abstract and non-abstract methods.
+•	It cannot be instantiated.
+•	It can have constructors and static methods also.
+•	It can have final methods that will force the subclass not to change the body of the method.
+Note: The phrase "instantiating a class" means the same thing as "creating an object." When you create an object, you are creating an "instance" of a class, therefore "instantiating" a class.
+
+Program- Abstraction
 abstract class Car
  {  
    abstract void run();  // No method body
@@ -12,7 +28,7 @@ class BMW extends Car
     
     public static void main(String args[])
     {
-       Car obj = new BMW();  // Upcasting (If the reference variable of Parent class refers to the object of Childclass, it is known as upcasting) 
+       Car obj = new BMW();  // Upcasting (If the reference variable of Parent class refers to the object of Child class, it is known as upcasting) 
        obj.run();  
     }  
 }  
